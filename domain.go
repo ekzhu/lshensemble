1e69a82fd6a3f16656f7562b44e2ff5436ac0ff5
Eric Zhu <erkangzhu@gmail.com>
2017-02-01 23:28:27 -0500

    add serialization methods for domain records

--- domain.go
+++ domain.go
@@ -1,6 +1,8 @@
 package lshensemble
 
 import (
+	"encoding/binary"
+	"io"
 	"sort"
 )
 
@@ -10,7 +12,7 @@ type Set struct {
 
 func NewSet() *Set {
 	return &Set{
-		Data : make(map[string]bool),
+		Data: make(map[string]bool),
 	}
 }
 
@@ -27,11 +29,79 @@ func (s *Set) Len() int {
 // DomainRecord represents a domain record.
 type DomainRecord struct {
 	// The unique key of this domain.
-	Key        string
+	Key string
 	// The domain size.
 	Size int
 	// The MinHash signature of this domain.
-	Signature  Signature
+	Signature Signature
+}
+
+// Write serializes the domain record and write to the data stream w, which implements
+// the io.Writer interface.
+// keyFn is used to convert the string key into bytes.
+// Write returns the number of bytes wrote and error if encountered any.
+func (r *DomainRecord) Write(w io.Writer, keyFn func(string) ([]byte, error)) (int, error) {
+	keyBin, err := keyFn(r.Key)
+	if err != nil {
+		return 0, err
+	}
+	n, err := w.Write(keyBin)
+	if err != nil {
+		return n, err
+	}
+	size := int64(r.Size)
+	if err := binary.Write(w, binary.LittleEndian, size); err != nil {
+		return n, err
+	}
+	n += 8
+	length := int32(len(r.Signature))
+	if err := binary.Write(w, binary.LittleEndian, length); err != nil {
+		return n, err
+	}
+	n += 4
+	for i := range r.Signature {
+		if err := binary.Write(w, binary.LittleEndian, r.Signature[i]); err != nil {
+			return n, err
+		}
+		n += 8
+	}
+	return n, nil
+}
+
+// Read deserializes the domain record from a data stream reader.
+// keySize is the number of bytes of the serialized key.
+// keyFn is used to convert the serialized key into string.
+// Read returns the number of bytes read and error if encountered any.
+func (r *DomainRecord) Read(reader io.Reader, keySize int, keyFn func([]byte) (string, error)) (int, error) {
+	keyBin := make([]byte, keySize)
+	n, err := reader.Read(keyBin)
+	if err != nil {
+		return n, err
+	}
+	key, err := keyFn(keyBin)
+	if err != nil {
+		return n, err
+	}
+	r.Key = key
+	var size int64
+	if err := binary.Read(reader, binary.LittleEndian, &size); err != nil {
+		return n, err
+	}
+	r.Size = int(size)
+	n += 8
+	var length int32
+	if err := binary.Read(reader, binary.LittleEndian, &length); err != nil {
+		return n, err
+	}
+	n += 4
+	r.Signature = make(Signature, int(length))
+	for i := range r.Signature {
+		if err := binary.Read(reader, binary.LittleEndian, &(r.Signature[i])); err != nil {
+			return n, err
+		}
+		n += 8
+	}
+	return n, nil
 }
 
 // A wrapper for sorting domains.
---
+++ domain_test.go
@@ -0,0 +1,38 @@
+package lshensemble
+
+import (
+	"bytes"
+	"testing"
+)
+
+func Test_Serialization(t *testing.T) {
+	key := "abcd"
+	size := 123
+	sig := randomSignature(8, int64(1))
+	record := &DomainRecord{
+		Key:       key,
+		Size:      size,
+		Signature: sig,
+	}
+	var data bytes.Buffer
+	n, err := record.Write(&data, func(s string) ([]byte, error) {
+		return []byte(s), nil
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("Number of bytes written to buffer", n)
+
+	var record2 DomainRecord
+	n, err = record2.Read(&data, 4, func(b []byte) (string, error) {
+		return string(b), nil
+	})
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log("Number of bytes read", n)
+	t.Log(record2)
+	if record2.Key != record.Key || record2.Size != record.Size {
+		t.Error("Incorrect record after deserialization")
+	}
+}
